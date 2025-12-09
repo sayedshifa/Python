a315bf67d0b47b271ed6c031e7f49f2122f5cd38
sayedshifa <sayedshifa@users.noreply.github.com>
2025-12-09 16:43:34 +0600

    Added main.py file

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+my_list=[1, 2, 3, 4, 5]
+print(my_list)
+my_list=my_list*2
+print(my_list)
+
+array=np.array([1,2,3,4])
+print(array)
+
+print(type(array))
+
+array=array*2
+print(array)
+
